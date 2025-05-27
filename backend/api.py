26fff7f535f24a3c51b94189b22185c640574f03
Marlon França <marlondiego407@gmail.com>
2025-05-27 20:32:39 -0300

    Adding cors

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from query import ask_question
 from typing import Dict, List
@@ -7,22 +8,28 @@ import uuid
 
 app = FastAPI()
 
+app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],  # Em produção, especifique o domínio do frontend
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 class QuestionRequest(BaseModel):
     thread_id: str = None
     question: str
 
 class AnswerResponse(BaseModel):
     answer: str
+    thread_id: str
 
 @app.post("/ask", response_model=AnswerResponse)
 def ask(request: QuestionRequest):
+    print("thread_id", request.thread_id)
     thread_id = request.thread_id
     if not request.thread_id:
         thread_id = str(uuid.uuid4())
-    # Busca histórico do DynamoDB
     history = get_thread_history(thread_id)
-    print(history)
     resposta = ask_question(request.question, history)
-    # Salva a nova interação
     save_message(thread_id, request.question, resposta)
     return {"answer": resposta, "thread_id": thread_id} 

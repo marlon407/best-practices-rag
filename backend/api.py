1e9676f3df0744af9ffaefb27a085d95b6d49497
Marlon França <marlondiego407@gmail.com>
2025-06-02 16:58:27 -0300

    english mfker

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,6 @@ from typing import Dict, List
 from dynamo_utils import save_message, get_thread_history
 import uuid
 
-
 app = FastAPI()
 
 app.add_middleware(
@@ -26,11 +25,10 @@ class AnswerResponse(BaseModel):
 
 @app.post("/ask", response_model=AnswerResponse)
 def ask(request: QuestionRequest):
-    print("thread_id", request.thread_id)
     thread_id = request.thread_id
     if not request.thread_id:
         thread_id = str(uuid.uuid4())
     history = get_thread_history(thread_id)
-    resposta = ask_question(request.question, history)
-    save_message(thread_id, request.question, resposta)
-    return {"answer": resposta, "thread_id": thread_id} 
+    answer = ask_question(request.question, history)
+    save_message(thread_id, request.question, answer)
+    return {"answer": answer, "thread_id": thread_id} 
